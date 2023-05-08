8719235718bcfee513f80a43df6cc3f357ffd338
dinshawiselinchristudhas <dinsha.wiselin@gmail.com>
2023-05-08 10:42:47 +0200

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Dinsha Wiselin Christudhas on 08.05.23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
